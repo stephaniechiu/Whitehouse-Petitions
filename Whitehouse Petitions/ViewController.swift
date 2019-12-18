cee5036db96a301f71e7b8884fa188cd68d9f8a3
Stephanie Chiu <stephaniechiu@outlook.com>
2019-12-17 19:42:11 -0800

    Initial Commit

---
+++ Whitehouse Petitions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Whitehouse Petitions
+//
+//  Created by Stephanie on 12/17/19.
+//  Copyright © 2019 Stephanie Chiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
