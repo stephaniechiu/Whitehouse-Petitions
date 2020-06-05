5bf6b3104238ef93de73a646f99348aaa76128a9
Stephanie Chiu <stephaniechiu@outlook.com>
2020-06-04 20:47:47 -0700

    Added concurrency to decrease response time.

--- Whitehouse Petitions/ViewController.swift
+++ Whitehouse Petitions/ViewController.swift
@@ -11,27 +11,31 @@ import UIKit
 class ViewController: UITableViewController {
      var petitions = [Petition]()
        
-       override func viewDidLoad() {
-           super.viewDidLoad()
-           
-        let urlString: String
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        performSelector(inBackground: #selector(fetchJSON), with: nil)
+    }
     
+    //Runs on background thread
+    @objc func fetchJSON() {
+        let urlString: String
         if navigationController?.tabBarItem.tag == 0 {
-            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+                urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=3"
         } else {
-           //will only show the most 100 popular petitions
-            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+               //will only show the most 100 popular petitions
+                urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
         }
-        
-            if let url = URL(string: urlString) {
-                if let data = try? Data(contentsOf: url){
-                    parse(json:data)
-                }
+            
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url){
+                parse(json: data) //called in background
             }
-            showError()
         }
-//Show error message if unable to parse JSON file
-    func showError() {
+            performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+    }
+    
+//Show error message if unable to parse JSON file. UIAlert needs to appear on main thread
+    @objc func showError() {
         let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
@@ -43,12 +47,16 @@ class ViewController: UITableViewController {
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
-            tableView.reloadData()
+            
+            //tableView will reload on main thread when load finishes
+        tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+        } else {
+            performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
         }
     }
 
 //Table View
-        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return petitions.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -65,5 +73,12 @@ class ViewController: UITableViewController {
         vc.detailItem = petitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
+    
+    @IBAction func creditsBtn(_ sender: Any) {
+        let creditsAlert = UIAlertController(title: "Credits", message: "These petitions were brought to you by the We The People API of the Whitehouse", preferredStyle: .alert)
+        let creditsAction = UIAlertAction(title: "Close", style: .default, handler: nil)
+        
+        creditsAlert.addAction(creditsAction)
+        present(creditsAlert, animated: true, completion: nil)
+    }
 }
-
